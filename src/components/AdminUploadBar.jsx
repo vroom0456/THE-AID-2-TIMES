2af0b88a1dd72cf79b2d820e005eae90684cdb7c
Varun Teja Cherukuthota <varuntejacherukuthota.0456@gmail.com>
2026-06-24 02:26:20 +0530

    Create AdminUploadBar.jsx

---
+++ src/components/AdminUploadBar.jsx
@@ -0,0 +1,22 @@
+// src/components/AdminUploadBar.jsx
+import React from 'react';
+import { saveSubjectResources } from '../utils/resourceManager';
+
+const AdminUploadBar = ({ subject, reg, branch, sem }) => {
+  const handleAdd = () => {
+    // Add your logic here to update subject.resources and call saveSubjectResources
+    alert("Resource added!");
+  };
+
+  return (
+    <div className="admin-upload-bar" style={{ border: "1px dashed gold", padding: "10px" }}>
+      <h4>⚡ Admin Upload</h4>
+      <input id="title" placeholder="Resource Title" />
+      <input id="url" placeholder="Google Drive Link" />
+      <button onClick={handleAdd}>Add to Vault</button>
+    </div>
+  );
+};
+
+export default AdminUploadBar;
+
