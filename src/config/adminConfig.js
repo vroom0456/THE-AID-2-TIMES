aab3b0e1e9f54ed4c4bd784169c934a732924ca4
Varun Teja Cherukuthota <varuntejacherukuthota.0456@gmail.com>
2026-06-24 02:23:31 +0530

    Create adminConfig.js

---
+++ src/config/adminConfig.js
@@ -0,0 +1,12 @@
+// src/config/adminConfig.js
+export const ADMIN_CREDS = [
+  btoa("varun:aid2times2024"),
+  btoa("teja:moderator2024"),
+  btoa("hannish:moderator2024")
+];
+
+export const ADMIN_NAMES = {
+  varun: "Varun Teja (Founder)",
+  teja: "Teja Praharsha",
+  hannish: "Hannish Reddi"
+};
