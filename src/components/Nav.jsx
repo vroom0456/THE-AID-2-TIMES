92e6bb4b260d3d42864788f1950f5130f11923e2
Varun Teja Cherukuthota <varuntejacherukuthota.0456@gmail.com>
2026-06-23 23:27:33 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,11 +5,11 @@
 import { useState } from "react";
 import { useUserStore, useUIStore } from "../store/useStore";
 
-export function Nav() {
+export default function Nav({ setModal }) {
   const user      = useUserStore((s) => s.user);
-  const setModal  = useUIStore((s) => s.setModalOpen);
   const setKBOpen = useUIStore((s) => s.setKBOpen);
   const [menuOpen, setMenuOpen] = useState(false);
+;
 
   let eggCount = 0;
   function eggClick() {
