63af20a0f27e79e1e1771089a46904fb5de334c3
Varun Teja Cherukuthota <varuntejacherukuthota.0456@gmail.com>
2026-06-24 16:30:01 +0530

    Add constants for branch codes, labels, and settings

---
+++ src/utils/constants.js
@@ -0,0 +1,29 @@
+export const BRANCH_CODES = [
+  "AIDS",
+  "CSE",
+  "AIML",
+  "IT",
+  "CIC",
+  "ECE",
+  "EVL",
+  "MECHANICAL",
+  "CIVIL",
+  "BIOTECH",
+];
+
+export const BRANCH_LABELS = {
+  AIDS: "AI & Data Science",
+  CSE: "Computer Science",
+  AIML: "AI & ML",
+  IT: "Information Technology",
+  CIC: "CS (IoT & Cyber)",
+  ECE: "Electronics & Communication",
+  EVL: "EV & VLSI",
+  MECHANICAL: "Mechanical",
+  CIVIL: "Civil",
+  BIOTECH: "Bio-Technology",
+};
+
+export const SEMESTERS = [1, 2, 3, 4, 5, 6, 7, 8];
+
+export const MAX_AVATAR_BYTES = 5 * 1024 * 1024;
