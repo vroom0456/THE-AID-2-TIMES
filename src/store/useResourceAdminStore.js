5efa94a2530f2f3934eab70077b8118ec684f00d
Varun Teja Cherukuthota <varuntejacherukuthota.0456@gmail.com>
2026-06-24 07:09:15 +0530

    Refactor useResourceAdminStore with async actions

--- src/store/useResourceAdminStore.js
+++ src/store/useResourceAdminStore.js
@@ -1,31 +1,118 @@
 import { create } from "zustand";
+import { supabase } from "../lib/supabase";
 
-export const useResourceAdminStore = create((set) => ({
-  adminMode: false,
+export const useResourceAdminStore =
+create((set) => ({
+adminMode: false,
 
-  selectedSubject: null,
-renameSubject: async (
-  subjectCode,
-  newName
-) => {
-  const { error } = await supabase
-    .from("resources")
-    .update({
-      subject_name: newName,
-    })
-    .eq("subject_code", subjectCode);
+selectedSubject: null,
 
-  return {
-    success: !error,
-    error,
-  };
-},
-  setAdminMode: (v) =>
-    set({ adminMode: v }),
+setAdminMode: (v) =>
+  set({
+    adminMode: v,
+  }),
 
-  selectSubject: (subject) =>
-    set({ selectedSubject: subject }),
+selectSubject: (
+  subject
+) =>
+  set({
+    selectedSubject:
+      subject,
+  }),
 
-  clearSubject: () =>
-    set({ selectedSubject: null }),
+clearSubject: () =>
+  set({
+    selectedSubject:
+      null,
+  }),
+
+renameSubject:
+  async (
+    subjectCode,
+    newName
+  ) => {
+    const {
+      error,
+    } = await supabase
+      .from(
+        "resources"
+      )
+      .update({
+        subject_name:
+          newName,
+      })
+      .eq(
+        "subject_code",
+        subjectCode
+      );
+
+    return {
+      success:
+        !error,
+      error,
+    };
+  },
+
+deleteSubject:
+  async (
+    subjectCode
+  ) => {
+    const {
+      error,
+    } = await supabase
+      .from(
+        "resources"
+      )
+      .delete()
+      .eq(
+        "subject_code",
+        subjectCode
+      );
+
+    return {
+      success:
+        !error,
+      error,
+    };
+  },
+
+addResource:
+  async (
+    resource
+  ) => {
+    const {
+      error,
+    } = await supabase
+      .from(
+        "resources"
+      )
+      .insert(
+        resource
+      );
+
+    return {
+      success:
+        !error,
+      error,
+    };
+  },
+
+deleteResource:
+  async (id) => {
+    const {
+      error,
+    } = await supabase
+      .from(
+        "resources"
+      )
+      .delete()
+      .eq("id", id);
+
+    return {
+      success:
+        !error,
+      error,
+    };
+  },
+
 }));
